fc5614e92825f9bd03de41db12cda306283ff168
MahmoudBdran <41477881+MahmoudBdran@users.noreply.github.com>
2024-04-07 14:48:22 +0200

    first commit

---
+++ PhotoAppApiAccountManagement/src/test/java/com/mbsystems/photoapp/api/accountmanagement/PhotoAppApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbsystems.photoapp.api.accountmanagement;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PhotoAppApiAccountManagementApplicationTests {
+
+	@Test
+    public void contextLoads() {
+	}
+
+}
---
+++ PhotoAppApiUsers/src/test/java/com/mbsystems/photoapp/api/users/PhotoAppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbsystems.photoapp.api.users;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PhotoAppApiUsersApplicationTests {
+
+	@Test
+    public void contextLoads() {
+	}
+
+}
